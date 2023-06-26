22e336acfcb03b91a03ab41f742a39dba9396653
Redahalojayen <136948571+Redahalojayen@users.noreply.github.com>
2023-06-26 10:26:18 +0300

    configure material ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React from 'react'
-import { Head } from 'next/document'
-import  Navbar  from '../../Components/Navbar'
 import Card from '../../Components/Card'
 const inter = Inter({ subsets: ['latin'] })
 
