c83abfc8db071f3d332f2499de829317fd56ba4b
Redahalojayen <136948571+Redahalojayen@users.noreply.github.com>
2023-07-05 08:58:14 +0300

    form page finshed needs to handle the request

--- src/pages/edit-game.tsx
+++ src/pages/edit-game.tsx
@@ -4,7 +4,7 @@ const editGame = () => {
   const [gameName, setGameName] = useState('');
   const [metacriticScore, setMetacriticScore] = useState('');
   const [playtime, setPlaytime] = useState('');
-  const [platforms, setPlatform] = useState([]);
+  const [platforms, setPlatforms] = useState([]);
 
   const handleGameNameChange = (event) => {
     setGameName(event.target.value);
@@ -19,7 +19,12 @@ const editGame = () => {
   };
 
   const handlePlatformsChange = (event) => {
-    setPlatform(event.target.value);
+    const { checked, value } = event.target;
+  if (checked) {
+    setPlatforms(platforms.concat(value));
+  } else {
+    setPlatforms(platforms.filter((platform) => platform !== value));
+  }
   };
 
   const handleSubmit = (event) => {
@@ -28,31 +33,79 @@ const editGame = () => {
   };
 
   return (
-   
-    <form className="max-w-lg mx-auto px-4 py-8 shadow-lg rounded-lg">
+   <div>
+ <form className="max-w-lg mx-auto px-4 py-8 shadow-lg rounded-lg">
       <div className="mb-4">
         <label htmlFor="gameName" className="block  font-bold mb-2">Game Name:</label>
-        <input type="text" id="gameName" value={gameName} onChange={handleGameNameChange} className="block w-full border border-gray-400 p-2 rounded-lg bg-[#E43F5A]" />
+        <input type="text" id="gameName" value={gameName} onChange={handleGameNameChange} className="border-none outline-none block w-full border border-gray-400 p-2 rounded-lg bg-[#E43F5A]" />
       </div>
       <div className="mb-4">
         <label htmlFor="metacriticScore" className="block  font-bold mb-2">Metacritic Score:</label>
-        <input type="text" id="metacriticScore" value={metacriticScore} onChange={handleMetacriticScoreChange} className="block w-full border bg-[#E43F5A] border-gray-400 p-2 rounded-lg" />
+        <input type="text" id="metacriticScore" value={metacriticScore} onChange={handleMetacriticScoreChange} className="border-none outline-none block w-full border bg-[#E43F5A] border-gray-400 p-2 rounded-lg" />
       </div>
       <div className="mb-4">
         <label htmlFor="playtime" className="block  font-bold mb-2">Playtime (hours):</label>
-        <input type="text" id="playtime" value={playtime} onChange={handlePlaytimeChange} className="block w-full border bg-[#E43F5A] border-gray-400 p-2 rounded-lg " />
+        <input type="number" id="playtime" value={playtime} onChange={handlePlaytimeChange} className="border-none outline-none block w-full border bg-[#E43F5A] border-gray-400 p-2 rounded-lg " />
       </div>
       <div className="mb-4">
         <label htmlFor="platforms" className="block  font-bold mb-2">Platforms:</label>
-        <select id="platforms" multiple value={platforms} onChange={handlePlatformsChange} className="block w-full border bg-[#E43F5A] border-gray-400 p-2 rounded-lg ">
-          <option value="playstation">Playstation</option>
-          <option value="xbox">Xbox</option>
-          <option value="pc">PC</option>
-          <option value="switch">Switch</option>
-        </select>
+       
+        <div className='flex justify-between'>
+        <label className={platforms.includes("playstation") ?  " bg-[#E43F5A]  p-2 rounded-xl w-28 text-center" : " bg-[#1E1E1E] p-2 rounded-xl w-28 text-center"}>
+    <input className ='hidden'
+      type="checkbox"
+      name="platforms"
+      value="playstation"
+      checked={platforms.includes("playstation")}
+      onChange={handlePlatformsChange}
+    />
+    Playstation
+  </label>
+  <label className={platforms.includes("xbox") ?  " bg-[#E43F5A]  p-2 rounded-xl w-28 text-center" : " bg-[#1E1E1E] p-2 rounded-xl w-28 text-center"}>
+  <input className ='hidden'
+
+      type="checkbox"
+      name="platforms"
+      value="xbox"
+      checked={platforms.includes("xbox")}
+      onChange={handlePlatformsChange}
+    />
+    Xbox
+  </label>
+  <label className={platforms.includes("pc") ? " bg-[#E43F5A]  p-2 rounded-xl w-28 text-center" : " bg-[#1E1E1E] p-2 rounded-xl w-28 text-center"}>
+
+  <input className ='hidden'
+
+      type="checkbox"
+      name="platforms"
+      value="pc"
+      checked={platforms.includes("pc")}
+      onChange={handlePlatformsChange}
+    />
+    PC
+  </label>
+  <label className={platforms.includes("switch") ?  " bg-[#E43F5A]  p-2 rounded-xl w-28 text-center" : " bg-[#1E1E1E] p-2 rounded-xl w-28 text-center"}>
+
+  <input className ='hidden'
+
+      type="checkbox"
+      name="platforms"
+      value="switch"
+      checked={platforms.includes("switch")}
+      onChange={handlePlatformsChange}
+    />
+    Switch
+  </label>
+</div>
+
       </div>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+      <div className='flex justify-end'>
+      <button type="submit" className="w-full bg-[#E43F5A] hover:bg-[#3b0f17] text-white font-bold py-2 px-4 rounded">Submit</button>
+      </div>
+      
     </form>
+   </div>
+   
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit', 
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], 
-  darkMode: false, 
+  media: false, 
   theme: { 
   extend: {}, 
   }, 
@@ -12,7 +11,7 @@ module.exports = {
   plugins: [], 
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
